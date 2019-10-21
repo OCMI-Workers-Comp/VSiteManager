57656000221165ecc285754b5a305245e21590d0
Cristian Herrera <cherreragiraldo@gmail.com>
2019-10-21 15:55:04 -0400

    Update nginx.sh

--- scripts/installers/nginx.sh
+++ scripts/installers/nginx.sh
@@ -4,12 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . $DIR/../../config
 
-#deb http://nginx.org/packages/ubuntu/ $RELEASE nginx
-#deb-src http://nginx.org/packages/ubuntu/ $RELEASE nginx
-
-#apt-get update
-#apt-get install nginx
-
 if [[ $NGINX_INSTALL = 1 ]]; then
    exit 1
 fi
